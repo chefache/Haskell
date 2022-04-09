d574e39d00a5fbc8229e5f6ec9a85103bfbe2377
Stefan Koev <stefan.m.koev@gmail.com>
2022-04-09 18:09:56 +0300

    Update FunctionsExercise.hs

--- FunctionsExercise.hs
+++ FunctionsExercise.hs
@@ -28,3 +28,19 @@ takeArrElement = ['S', 't', 'e', 'f', 'a', 'n'] !! 0 -- return 'S'
 takeStrElement = "Stefan" !! 2 -- return 'e'
 
 listOfLists = [[1, 2, 3], [4, 5, 6]] !! 0
+
+particularListElements1 = [1,5..200] -- start from 1 and take all 5th elements to 200
+
+particularListElements2 = [2,8..20] -- start from 2 and take all 6th element to 20
+
+reverseList1 = [20,19..1] -- return list with numbers frome 20 to 1 
+
+reverseList2 = [100, 95..1] --return list with every 5th number from 100 to 1 
+
+takeFirst24MultipliesOf13 = take 24 [13,26..] -- return first 24 multypliers of 13 (26 = 2 * 13) => 13, 26, 39, 52...312
+
+takeFirst100MultiplyersOf5 = take 100[5,20..] -- return 5,20,35,50,65....1490
+
+cycleArr = take 15 (cycle [1,2,3,4]) -- it wil return [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3]
+
+repeatArr = take 10(repeat 5) -- it will return [5,5,5,5,5,5,5,5,5,5]
